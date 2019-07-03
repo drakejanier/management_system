aaca336d65420a5506217578bc1876e2fb29f433
Jerome Janier <jeromejanier2@gmail.com>
2019-07-03 20:06:17 +0800

    0703-02-06

--- sales/urls.py
+++ sales/urls.py
@@ -3,10 +3,11 @@ from .views import *
 from . import views
 
 urlpatterns = [
-    path('register/', views.SalesView, name='sales-register'),
+    path('register/<int:pk>/', views.SalesView, name='sales-register'),
     
     path('list-item-delete/<int:pk>/', views.ListItemDelete, name='list-item-delete'),
     path('list-item-add/<int:pk>/', views.itemAdd, name='list-item-add'),
     path('list-item-deduct/<int:pk>/', views.itemDeduct, name='list-item-deduct'),
+    
     # url(r'^items-autocomplete/$', getItemList.as_view(), name='items-autocomplete',), #move to inventory
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,7 +1,7 @@
 
 from django.contrib import messages
 from django.db.models import Sum
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, FormView, View
 from django.urls import reverse_lazy
 from .models import Sales, tempSalesList, SalesList
@@ -11,15 +11,15 @@ from shapeshifter.views import MultiFormView
 
 # Create your views here.
 
-class SalesViewSolo(CreateView): #TEMP check if SalesVIEW is working
-    template_name = 'sales/register.html'    
-    # model = Sales
-    form_class = SalesForm    
-    context_object_name = 'sales'
-    
-def SalesView(request):
+def SalesView(request, pk):
     sales_form = SalesForm()
-    saleslist_form = SalesListForm()
+    
+    if pk!=0:
+        product_item = get_object_or_404(Products, pk=pk)
+        saleslist_form = SalesListForm(initial={'Item': product_item})
+    else:
+        saleslist_form = SalesListForm()
+    
     added_item = tempSalesList.objects.all()
     product_count = tempSalesList.objects.count()
     qty = tempSalesList.objects.all().aggregate(total_qty=Sum('Quantity'))  
@@ -58,7 +58,7 @@ def SalesView(request):
                     savelist.Item = confirm_item
                     savelist.save() # <<<<<<<<<<<<<<<<<<< item add SUCCESS HERE
                     messages.success(request, "added")   
-                    return redirect('sales-register')
+                    return redirect('sales-register', 0)
 
                 else:
                     messages.info(request, f"Invalid Quantity. Maximum Quantity is {item_qty}")                 
@@ -98,7 +98,7 @@ def SalesView(request):
                     
                 tempSalesList.objects.all().delete() # templist DELETED
                 messages.success(request, f'Items Saved')
-                return redirect('sales-register')
+                return redirect('sales-register', 0)
 
             else:
                 messages.info(request, f"Invalid form {sales_info.errors}. {total_qty}")
@@ -118,7 +118,7 @@ def ListItemDelete(request, pk):
     item = tempSalesList.objects.only('Item').get(pk=pk).Item
     messages.success(request, f'{item} deleted.')      
     tempSalesList.objects.filter(id=pk).delete()    
-    return redirect('sales-register')
+    return redirect('sales-register', 0)
 
 def itemAdd(request, pk):
     item = tempSalesList.objects.get(pk=pk)
@@ -134,7 +134,7 @@ def itemAdd(request, pk):
     else:
         messages.info(request, 'max qty reached')
     
-    return redirect('sales-register')
+    return redirect('sales-register', 0)
 
 def itemDeduct(request, pk):
     item = tempSalesList.objects.get(pk=pk)
@@ -148,4 +148,4 @@ def itemDeduct(request, pk):
     else:
         messages.info(request, 'min qty reached')
     
-    return redirect('sales-register')
+    return redirect('sales-register', 0)
